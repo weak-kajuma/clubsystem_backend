ade01c99c1bbc43118fb29548ad6fd4df0958c76
weak-kajuma <96107127+weak-kajuma@users.noreply.github.com>
2022-09-24 23:09:11 +0900

    change root path

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from starlette.middleware.cors import CORSMiddleware
 from api.routers import login
 
-app = FastAPI(root_path="/todo/v1")
+app = FastAPI(root_path="/clubsystem/v1")
 
 app.add_middleware(
     CORSMiddleware,
